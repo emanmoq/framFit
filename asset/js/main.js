f3e93267e47640aabea54de1b5baf378c8155850
emoq <e.moq2005@gmail.com>
2019-07-30 15:00:42 +0300

    gg

---
+++ asset/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    
+    $('.mallCarusel').owlCarousel({
+        autoplay: true,
+        rtl:true,
+        loop: true,
+        dots:true,
+        nav:true,
+        navText: ["<i class='lni-chevron-right'></i>","<i class='lni-chevron-left'></i>"],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items:1,
+      
+            }
+          
+        }
+  
+
+        
+});
+
+});
+
