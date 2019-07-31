83554f4385a05eb3681d6db7f7616e5ac3cfbe40
emanmoq <e.moq2005@gmail.com>
2019-07-31 17:21:37 +0300

    mm

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,24 +1,78 @@
 $(document).ready(function () {
-    
-    $('.mallCarusel').owlCarousel({
-        autoplay: true,
-        rtl:true,
-        loop: true,
-        dots:true,
-        nav:true,
-        navText: ["<i class='lni-chevron-right'></i>","<i class='lni-chevron-left'></i>"],
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items:1,
-      
-            }
-          
-        }
   
+    var iCal = new iCalendar("#Calender");
+    iCal.render();
+    
 
-        
-});
+    $(window).scroll(function () {
+        scrollTop = $(window).scrollTop();
 
+         if( scrollTop >$('#menu').height()){
+            $('#menu').addClass('scrollNav');
+
+
+         }
+         else{
+            $('#menu').removeClass('scrollNav');    
+
+         }
+         
+   
+        });
+        $(".SignClick").click(function() {
+            $(".signInModal").css("display","block");
+           });
+           $(".signInModal").click(function() {
+            $(this).css("display","none");
+           });
+        $(".nav-link").click(function() {
+            $(".nav-link").removeClass("aciveLink");
+           $(this).addClass("aciveLink");
+           });
+           $(".Features-Link").click(function() { 
+	
+            $('.nav-link').removeClass('activeLink');
+            $(this).addClass('activeLink');
+            
+                $('html, body').animate({
+                    scrollTop: $("#Features").offset().top-(150)
+                }, 1000);
+            });
+            $(".Community-Link").click(function() { 
+	
+                $('.nav-link').removeClass('activeLink');
+                $(this).addClass('activeLink');
+                
+                    $('html, body').animate({
+                        scrollTop: $("#Community").offset().top-(150)
+                    }, 1000);
+                });
+                $(".GetStartSection-Link").click(function() { 
+	
+                    $('.nav-link').removeClass('activeLink');
+                    $(this).addClass('activeLink');
+                    
+                        $('html, body').animate({
+                            scrollTop: $("#GetStartSection").offset().top-(150)
+                        }, 1000);
+                    });
+                    $(".ClientSign").click(function() {
+                        $(".signInSign").css("display","block");
+                       });
+                       $(".BusniessSign").click(function() {
+                        $(".signInSign").css("display","block");
+                       });
+                     
+                       $(".SignIn.SignUp").click(function() {
+                        $(".signInUp").css("display","block");
+                       });
+                       $(".nav-link").click(function() { 
+	
+                        $('.nav-link').removeClass('activeLink');
+                        $(this).addClass('activeLink');
+                        
+                        });
+                  
+                
 });
 
